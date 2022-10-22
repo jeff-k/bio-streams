38beb1e923a8aff36fab06e9bba8a38d27acb55c
jeff-k <jeff_k@fastmail.com>
2022-10-22 15:42:30 +0100

    non-borrowing, non-streaming iterator

--- README.md
+++ README.md
@@ -1,1 +1,18 @@
 # bio-streams
+
+todo:
+
+Type safety:
+* quality score trait, implement phred
+
+Async:
+* futures::streams for async
+
+GAT in-place memory track:
+&[u8] should deref coerce to &Vec<Phred>
+* deref coercion for bio-seq Seq<...>
+
+
+other:
+* benchmark
+* examples
--- src/fastq.rs
+++ src/fastq.rs
@@ -1,39 +1,97 @@
-extern crate flate2;
-extern crate futures;
+use core::marker::PhantomData;
+use std::fmt;
+use std::io::BufRead;
+use std::str;
 
-use std::fs::File;
-//use std::io::Bufreader;
-use std::path::Path;
+//use futures::{StreamExt, Stream, AsyncRead};
+//use futures::task::{Context, Poll};
+//use futures::io::BufReader;
 
-use flate2::read::MultiGzDecoder;
-use futures::{StreamExt, Stream, AsyncRead};
-use futures::io::BufReader;
-
-pub struct Record<T> {
-    name: Vec<u8>,
-    seq: T,
-    quality: Vec<u8>,
-    info: Option<Vec<u8>>,
+pub struct Record<T = Vec<u8>> {
+    pub fields: Vec<u8>,
+    pub seq: T,
+    pub quality: Vec<u8>,
 }
 
-pub struct Fastq<T> {
-    buf: BufReader<T>,
+impl fmt::Display for Record {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(
+            f,
+            "{}\t{}",
+            str::from_utf8(&self.fields).unwrap(),
+            str::from_utf8(&self.seq).unwrap()
+        )
+    }
 }
 
-impl<T: AsyncRead> Fastq<T> {
-    fn open(path: Path) -> Self {
-        if path.ends_with(".gz") {
-            Fastq { buf: BufReader::<T>::new(MultiGzDecoder::new(File::open(path).unwrap())) }
-        } else {
-            Fastq { buf: BufReader::<T>::new(File::open(path).unwrap()) }
+impl Record {
+    pub fn data_fields(self) -> Vec<Vec<u8>> {
+        //        self.fields.split(' ')
+        unimplemented!()
+    }
+
+    pub fn name(self) -> Vec<u8> {
+        unimplemented!()
+    }
+
+    pub fn description(self) -> Option<Vec<u8>> {
+        unimplemented!()
+    }
+}
+
+pub struct Fastq<R: BufRead, T = Vec<u8>> {
+    buf: R,
+    p: PhantomData<T>,
+}
+
+impl<R: BufRead, T> Fastq<R, T> {
+    pub fn new(buf: R) -> Self {
+        Fastq {
+            buf,
+            p: PhantomData,
         }
     }
-
-    // from buffer
-    fn new(bytes: &[u8]) -> Self {
-        unimplemented!();
-    }
 }
 
-impl<T> Stream for Fastq<T> {
+//impl<R: BufRead, T> Fastq<R, T> {
+//    pub fn from_fq(path: &Path) -> Fastq<BufReader<File>, T> {
+//        Fastq { buf: BufReader::new(File::open(path).unwrap()), p: PhantomData }
+//    }
+//}
+
+impl<R: BufRead, T: From<Vec<u8>>> Iterator for Fastq<R, T> {
+    type Item = Record<Vec<u8>>;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        let mut name: Vec<u8> = Vec::new();
+        let mut seq: Vec<u8> = Vec::new();
+        let mut delim: Vec<u8> = Vec::new();
+        let mut quality: Vec<u8> = Vec::new();
+
+        // this includes the newline
+        let n_bs = self.buf.read_until(b'\n', &mut name).unwrap();
+        if n_bs == 0 {
+            // the stream has properly ended
+            return None;
+        }
+        assert_eq!(name[0], b'@');
+
+        let s_bs = self.buf.read_until(b'\n', &mut seq).unwrap();
+
+        // one option is to just consume the +\n line:
+        //self.buf.consume(2);
+
+        let d_bs = self.buf.read_until(b'\n', &mut delim).unwrap();
+        assert_eq!(d_bs, 2);
+
+        let q_bs = self.buf.read_until(b'\n', &mut quality).unwrap();
+        // read equal number of bytes for sequence and quality string
+        assert_eq!(s_bs, q_bs);
+
+        Some(Record {
+            fields: name[..(n_bs - 1)].to_vec(),
+            seq: seq[..(s_bs - 1)].into(),
+            quality: quality[..(q_bs - 1)].to_vec(),
+        })
+    }
 }
