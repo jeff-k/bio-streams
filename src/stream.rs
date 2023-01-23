846b93dc771e2963965fa1d828e8630a6c371f0d
jeff-k <jeff_k@fastmail.com>
2023-01-23 22:31:15 +0000

    compiles, phantomtype needed to be unpinnable. incomplete parsing

--- src/stream.rs
+++ src/stream.rs
@@ -22,6 +22,8 @@ pub struct Fastq<R: BufRead, T = Vec<u8>> {
     p: PhantomData<T>,
 }
 
+impl<R: BufRead + Unpin, T: From<Vec<u8>> + Unpin> Unpin for Fastq<R, T> {}
+
 impl<R: BufRead + Into<Box<R>>, T> Fastq<R, T> {
     pub fn new(buf: R) -> Self {
         Fastq {
@@ -31,131 +33,31 @@ impl<R: BufRead + Into<Box<R>>, T> Fastq<R, T> {
     }
 }
 
-/*
-impl<R: BufRead, T: From<Vec<u8>> + Unpin> Stream for Fastq<R, T> {
+impl<R: BufRead + Unpin, T: From<Vec<u8>> + Unpin> Stream for Fastq<R, T> {
     type Item = Record<T>;
 
     fn poll_next(
         self: Pin<&mut Self>,
-        cx: &mut Context<'_>,
+        _cx: &mut Context<'_>,
     ) -> Poll<Option<<Self as Stream>::Item>> {
-
-        let ref b = self.get_mut().buf.as_mut();
-
-
+        //let mut r = unsafe { self.as_mut().map_unchecked_mut(|this| &mut this.buf) };
 
         let mut x: Vec<u8> = Vec::new();
-        let nbs = b.read_until(b'\n', &mut x);
+        let r = unsafe {
+            self.get_unchecked_mut()
+                .buf
+                .as_mut()
+                .read_until(b'\n', &mut x)
+        };
 
-        Poll::Ready(None)
-    }
-}
-*/
-
-/*
-    //let n_bs = self.get_mut()
-    //    .buf
-    //    .as_mut()
-    match n1.read_until(b'\n', &mut name) {
-        Ok(0) => task::Poll::Ready(None),
-        Ok(n) => task::Poll::Ready(Some(Record {
-            fields: name,
-            seq: vec![].into(),
-            //          seq: seq.into(),
-            quality: None,
-            //            quality: Some(quality),
-        })),
-
-        _ => task::Poll::Ready(None),
-    }
-}
-        if n_bs == 0 {
-            // the stream has properly ended
-            return task::Poll::Ready(None);
-        }
-        //        assert_eq!(name[0], b'@');
-        let s_bs = self.buf.read_until(b'\n', &mut seq).unwrap();
-
-        // one option is to just consume the +\n line:
-        //self.buf.consume(2);
-
-        let d_bs = self.buf.read_until(b'\n', &mut delim).unwrap();
-        assert_eq!(d_bs, 2);
-
-        let q_bs = self.buf.read_until(b'\n', &mut quality).unwrap();
-        // read equal number of bytes for sequence and quality string
-        //        assert_eq!(s_bs, q_bs);
-        name.truncate(n_bs - 1);
-//        seq.truncate(s_bs - 1);
-//        quality.truncate(q_bs - 1);
-
-        task::Poll::Ready(Some(Record {
-            fields: name,
-            seq: vec![].into(),
-            //          seq: seq.into(),
-            quality: None,
-//            quality: Some(quality),
-        }))
-    }
-    */
-
-/*
-impl<R: BufRead, T: From<Vec<u8>> + Unpin> Stream for Fastq<R, T> {
-    type Item = Record<T>;
-
-    fn poll_next(
-        self: Pin<&mut Self>,
-        cx: &mut task::Context<'_>,
-    ) -> task::Poll<Option<<Self as Stream>::Item>> {
-        let mut name: Vec<u8> = Vec::new();
-        //        let mut seq: Vec<u8> = Vec::new();
-        //        let mut delim: Vec<u8> = Vec::new();
-        //        let mut quality: Vec<u8> = Vec::new();
-
-        let n1 = self.get_mut().buf.as_mut();
-
-        //let n_bs = self.get_mut()
-        //    .buf
-        //    .as_mut()
-        match n1.read_until(b'\n', &mut name) {
-            Ok(0) => task::Poll::Ready(None),
-            Ok(n) => task::Poll::Ready(Some(Record {
-                fields: name,
-                seq: vec![].into(),
-                //          seq: seq.into(),
+        match r {
+            Ok(0) => Poll::Ready(None),
+            Ok(_n) => Poll::Ready(Some(Record {
+                fields: vec![].into(),
+                seq: x.into(),
                 quality: None,
-                //            quality: Some(quality),
             })),
-
-            _ => task::Poll::Ready(None),
+            _ => Poll::Ready(None),
         }
     }
-            if n_bs == 0 {
-                // the stream has properly ended
-                return task::Poll::Ready(None);
-            }
-            //        assert_eq!(name[0], b'@');
-            let s_bs = self.buf.read_until(b'\n', &mut seq).unwrap();
-
-            // one option is to just consume the +\n line:
-            //self.buf.consume(2);
-
-            let d_bs = self.buf.read_until(b'\n', &mut delim).unwrap();
-            assert_eq!(d_bs, 2);
-
-            let q_bs = self.buf.read_until(b'\n', &mut quality).unwrap();
-            // read equal number of bytes for sequence and quality string
-            //        assert_eq!(s_bs, q_bs);
-            name.truncate(n_bs - 1);
-    //        seq.truncate(s_bs - 1);
-    //        quality.truncate(q_bs - 1);
-
-            task::Poll::Ready(Some(Record {
-                fields: name,
-                seq: vec![].into(),
-                //          seq: seq.into(),
-                quality: None,
-    //            quality: Some(quality),
-            }))
-        }
-        */
+}
