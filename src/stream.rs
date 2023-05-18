fe2319ba7ce30abffda75c3b098dde8950af24da
jeff-k <jeff_k@fastmail.com>
2023-05-18 16:29:37 +0100

    stash changes to streaming results

--- src/stream.rs
+++ src/stream.rs
@@ -1,19 +1,12 @@
-//extern crate futures;
-
 use core::marker::PhantomData;
-//use futures::io::AsyncBufRead;
-//use futures::AsyncBufReadExt;
 
 use std::task::{Context, Poll};
 
 use std::marker::Unpin;
 use std::pin::Pin;
 
-//use futures::io::BufReader;
-//use futures::stream::{Stream, StreamExt};
 use futures::stream::Stream;
 use std::io::BufRead;
-//use std::io::BufReader;
 
 use crate::Record;
 
@@ -40,8 +33,6 @@ impl<R: BufRead + Unpin, T: From<Vec<u8>> + Unpin> Stream for Fastq<R, T> {
         self: Pin<&mut Self>,
         _cx: &mut Context<'_>,
     ) -> Poll<Option<<Self as Stream>::Item>> {
-        //let mut r = unsafe { self.as_mut().map_unchecked_mut(|this| &mut this.buf) };
-
         let mut fields: Vec<u8> = Vec::new();
         let mut seq: Vec<u8> = Vec::new();
         let mut sep: Vec<u8> = Vec::new();
@@ -54,11 +45,11 @@ impl<R: BufRead + Unpin, T: From<Vec<u8>> + Unpin> Stream for Fastq<R, T> {
                 .map_err(|e| e.to_string())?;
             x.read_until(b'\n', &mut seq).map_err(|e| e.to_string())?;
             x.read_until(b'\n', &mut sep).map_err(|e| e.to_string())?;
-            x.read_until(b'\n', &mut quality)
-                .map_err(|e| e.to_string())?
+            x.read_until(b'\n', &mut quality).map_err(|e| e.to_string())
         };
+
         match r {
-            Ok(0) => Poll::Ready(None),
+            Ok(n) if n == 0 => Poll::Ready(None),
             Ok(_n) => Poll::Ready(Some(Ok(Record {
                 fields: fields.into(),
                 seq: seq.into(),
